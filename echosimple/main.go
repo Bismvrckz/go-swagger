ec20630fa0c5ae7650eccce03f3bd5a32337e7a7
Ahmad  Syamil <ahmadfayruzsyamil33@gmail.com>
2023-08-21 14:56:10 +0700

    doc

--- echosimple/main.go
+++ echosimple/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
-	_ "github.com/docs/echosimple"
+	// _ "github.com/Bismvrckz/go-swagger.git/docs/echosimple"
 	// you need to update github.com/rizalgowandy/go-swag-sample with your own project path
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
