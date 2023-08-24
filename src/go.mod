4e77ecdbe1246ba0d0ba9fbe6b26edf1b66df47f
Ahmad  Syamil <ahmadfayruzsyamil33@gmail.com>
2023-08-24 18:28:26 +0700

    keycloak go (not gocloak)

--- src/go.mod
+++ src/go.mod
@@ -3,18 +3,22 @@ module echosimple
 go 1.21.0
 
 require (
+	github.com/coreos/go-oidc/v3 v3.6.0
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/swaggo/echo-swagger v1.4.0
 	github.com/swaggo/swag v1.16.1
+	golang.org/x/oauth2 v0.11.0
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.0 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/spec v0.20.9 // indirect
 	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -30,5 +34,7 @@ require (
 	golang.org/x/text v0.12.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	// "github.com/labstack/echo/v4"
+	// "github.com/labstack/echo/v4/middleware"
+
+	// _ "echosimple/docs"
+
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	// echoSwagger "github.com/swaggo/echo-swagger"
 
-	_ "echosimple/docs"
-
-	echoSwagger "github.com/swaggo/echo-swagger"
+	oidc "github.com/coreos/go-oidc/v3/oidc"
+	"golang.org/x/oauth2"
 )
 
 // import "devGo/docs"
@@ -30,19 +37,110 @@ import (
 // @schemes http
 func main() {
 	// Echo instance
-	e := echo.New()
+	configURL := "http://localhost:8080/realms/demo"
+	// http://localhost:8080/realms/demo/.well-known/openid-configuration
+	// e := echo.New()
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, configURL)
+
+	if err != nil {
+		panic(err)
+	}
+
+	clientID := "demo-client"
+	clientSecret := "PF9hF9fhQQGsZdY5FgAIMEuVg9tauumd"
+
+	redirectURL := "http://localhost:3000/demo/callback"
+	// Configure an OpenID Connect aware OAuth2 client.
+	oauth2Config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		// Discovery returns the OAuth2 endpoints.
+		Endpoint: provider.Endpoint(),
+		// "openid" is a required scope for OpenID Connect flows.
+		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+	state := "somestate"
+
+	oidcConfig := &oidc.Config{
+		ClientID: clientID,
+	}
+	verifier := provider.Verifier(oidcConfig)
 
 	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.CORS())
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+	// e.Use(middleware.CORS())
 
 	// Routes
-	e.GET("/", HealthCheck)
-	e.GET("/swagger/*", echoSwagger.WrapHandler)
+	// e.GET("/health", HealthCheck)
+	// e.GET("/swagger/*", echoSwagger.WrapHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rawAccessToken := r.Header.Get("Authorization")
+		if rawAccessToken == "" {
+			http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
+			return
+		}
+
+		parts := strings.Split(rawAccessToken, " ")
+		if len(parts) != 2 {
+			w.WriteHeader(400)
+			return
+		}
+		_, err := verifier.Verify(ctx, parts[1])
+
+		if err != nil {
+			http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
+			return
+		}
+
+		w.Write([]byte("hello world"))
+	})
+
+	http.HandleFunc("/demo/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := oauth2Config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+			return
+		}
+		idToken, err := verifier.Verify(ctx, rawIDToken)
+		if err != nil {
+			http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			OAuth2Token   *oauth2.Token
+			IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
+		}{oauth2Token, new(json.RawMessage)}
+
+		if err := idToken.Claims(&resp.IDTokenClaims); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+	})
+
+	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 
 	// Start server
-	e.Logger.Fatal(e.Start(":3000"))
+	// e.Logger.Fatal(e.Start(":3000"))
 }
 
 // HealthCheck godoc
@@ -53,8 +151,8 @@ func main() {
 // @Produce json
 // @Success 200 {object} map[string]interface{}
 // @Router / [get]
-func HealthCheck(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"data": "Server is up and running",
-	})
-}
+// func HealthCheck(c echo.Context) error {
+// 	return c.JSON(http.StatusOK, map[string]interface{}{
+// 		"data": "Server is up and running",
+// 	})
+// }
