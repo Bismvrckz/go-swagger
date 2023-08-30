78d9785fb4638987c0c93daf16e5719a7fa00ecf
Ahmad  Syamil <ahmadfayruzsyamil33@gmail.com>
2023-08-30 15:00:00 +0700

    echo login

--- src/go.mod
+++ src/go.mod
@@ -4,10 +4,8 @@ go 1.21.0
 
 require (
 	github.com/Nerzal/gocloak/v13 v13.8.0
-	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/swaggo/swag v1.16.1
-	golang.org/x/oauth2 v0.11.0
 )
 
 require (
@@ -19,7 +17,6 @@ require (
 	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -27,7 +24,6 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pquerna/cachecontrol v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
@@ -38,8 +34,5 @@ require (
 	golang.org/x/text v0.12.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
+	"context"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
 	// _ "echosimple/docs"
-	"context"
-	"net/http"
-	"strings"
 
 	// echoSwagger "github.com/swaggo/echo-swagger"
+	"net/http"
 
 	"github.com/Nerzal/gocloak/v13"
-	oidc "github.com/coreos/go-oidc"
-	"golang.org/x/oauth2"
 )
 
-// import "devGo/docs"
-
-// @title Echo Swagger Example API
-// @version 1.0
-// @description This is a sample server server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:3000
-// @BasePath /
-// @schemes http
 func main() {
 
 	// http://localhost:8080/realms/demo/.well-known/openid-configuration
@@ -45,58 +26,32 @@ func main() {
 
 	// Routes
 	e.GET("/health", HealthCheck)
-	// e.GET("/swagger/*", echoSwagger.WrapHandler)
-	e.GET("/login", HandleKeycloakLogin)
+	e.POST("/login", Login)
 
-	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
-func HandleKeycloakLogin(c echo.Context, w http.ResponseWriter, r *http.Request) error {
-	// w := http.ResponseWriter
-	// r := *http.Request
-	clientID := "demo-client"
-	clientSecret := "cbfd6e04-a51c-4982-a25b-7aaba4f30c81"
-	realm := "demo"
-	username := "demo"
-	password := "demo"
+func HealthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"data": "Server is up and running",
+	})
+}
+
+func Login(c echo.Context) error {
 	client := gocloak.NewClient("http://localhost:8080")
-	configURL := "http://localhost:8080/auth/realms/demo"
 	ctx := context.Background()
-	provider, err := oidc.NewProvider(ctx, configURL)
+	username := c.FormValue("user")
+	password := c.FormValue("pass")
+	clientSecret := "PF9hF9fhQQGsZdY5FgAIMEuVg9tauumd"
+	clientID := "demo-client"
+	realm := "demo"
 
-	if err != nil {
-		panic(err)
-	}
-
-	redirectURL := "http://localhost:8181/demo/callback"
-	oauth2Config := oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		RedirectURL:  redirectURL,
-		// Discovery returns the OAuth2 endpoints.
-		Endpoint: provider.Endpoint(),
-		// "openid" is a required scope for OpenID Connect flows.
-		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
-	}
-	// code := c.QueryParam("code")
-
-	state := "somestate"
-
-	oidcConfig := &oidc.Config{
-		ClientID: clientID,
-	}
-
-	verifier := provider.Verifier(oidcConfig)
-
-	// jwt, err := client.LoginClientTokenExchange(context.Background(), clientID, token.AccessToken, clientSecret, realm, "demo". "633f9c89-cd86-4ddc-b10c-8bcbef52cb4c")
-
-	jwt, err := client.Login(ctx, clientID, clientSecret, realm, username, password)
+	token, err := client.Login(ctx, clientID, clientSecret, realm, username, password)
 	if err != nil {
 		panic("Something wrong with the credentials or url")
 	}
 
-	rptResult, err := client.RetrospectToken(ctx, jwt.AccessToken, clientID, clientSecret, realm)
+	rptResult, err := client.RetrospectToken(ctx, token.AccessToken, clientID, clientSecret, realm)
 	if err != nil {
 		panic("Inspection failed:" + err.Error())
 	}
@@ -105,32 +60,5 @@ func HandleKeycloakLogin(c echo.Context, w http.ResponseWriter, r *http.Request)
 		panic("Token is not active")
 	}
 
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "Failed to exchange code for tokens")
-	}
-
-	rawAccessToken := r.Header.Get("Authorization")
-	if rawAccessToken == "" {
-		http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
-	}
-
-	parts := strings.Split(rawAccessToken, " ")
-	if len(parts) != 2 {
-		w.WriteHeader(400)
-		return w.Write([]byte("hello world"))
-	}
-	a, err := verifier.Verify(ctx, parts[1])
-
-	if err != nil {
-		http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
-		return
-	}
-
-	return w.Write([]byte("hello world"))
-}
-
-func HealthCheck(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"data": "Server is up and running",
-	})
+	return c.JSON(http.StatusOK, rptResult)
 }
